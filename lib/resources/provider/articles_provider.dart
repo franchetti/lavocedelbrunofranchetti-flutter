a189ab2756198d71ab2b563b880508d87d2bf267
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-04-03 13:32:17 +0200

    Corretto un errore nel parsing dei salvati.
    Took 2 minutes

--- lib/resources/provider/articles_provider.dart
+++ lib/resources/provider/articles_provider.dart
@@ -34,7 +34,7 @@ class ArticlesProvider {
     }
     debugPrint("Cerco gli articoli salvati: " + preferences.savedPosts.toString() + ".");
 
-    List<Post> rawArticles = await References.wordPress.fetchPosts(postParams: ParamsPostList(includePostIDs: preferences.savedPosts));
+    List<Post> rawArticles = await References.wordPress.fetchPosts(postParams: ParamsPostList(includePostIDs: preferences.savedPosts), fetchFeaturedMedia: true);
 
     List<ArticleModel> articles = List<ArticleModel>();
     rawArticles.forEach((Post rawArticle) => articles.add(ArticleModel.fromWordpressPost(rawArticle)));
