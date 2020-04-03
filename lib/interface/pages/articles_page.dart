eb754335613c7359c5f521e08315b0a7952396a6
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-04-03 19:58:39 +0200

    Aggiunta la appbar con immagine.
    Took 22 minutes

--- lib/interface/pages/articles_page.dart
+++ lib/interface/pages/articles_page.dart
@@ -17,7 +17,15 @@ class ArticlesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: References.appBar(context, S.of(context).appName),
+      appBar: AppBar(
+        title: Image(
+          image: Images.logoUfficiale,
+          fit: BoxFit.fitHeight,
+          height: 40.0,
+        ),
+        centerTitle: true,
+        // elevation: 0,
+      ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
         child: ListView.separated(
@@ -25,7 +33,6 @@ class ArticlesPage extends StatelessWidget {
               ? ArticleListElement(article: articles.elementAt(index), preferences: preferences)
               : ArticleListElement.reduced(article: articles.elementAt(index), preferences: preferences),
           separatorBuilder: (BuildContext context, int index) => Divider(),
-          // TODO: Sostituire con il dato preconosciuto.
           itemCount: articles.length,
         ),
       ),
