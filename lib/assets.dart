8e33584c0d0960f4bf4a3de87e40957a0d286d6b
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-04-07 13:01:11 +0200

    Riformattato il codice.
    Took 34 seconds

--- lib/assets.dart
+++ lib/assets.dart
@@ -3,9 +3,10 @@ import 'package:flutter/widgets.dart';
 class Images {
   /// ![](file:///home/emiliodallatorre/Documenti/GitHub/traileoni-flutter/assets/ic_extended.png)
   static AssetImage get icExtended => const AssetImage("assets/ic_extended.png");
+
   /// ![](file:///home/emiliodallatorre/Documenti/GitHub/traileoni-flutter/assets/ic_extended_dark.png)
   static AssetImage get icExtendedDark => const AssetImage("assets/ic_extended_dark.png");
+
   /// ![](file:///home/emiliodallatorre/Documenti/GitHub/traileoni-flutter/assets/ic_launcher.png)
   static AssetImage get icLauncher => const AssetImage("assets/ic_launcher.png");
 }
-
