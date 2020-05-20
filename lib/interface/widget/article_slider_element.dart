6b7999b7c8041b5c19ccb51d982cd06e0d3f8d37
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-05-20 18:46:20 +0200

    Reso clickabile l'articolo nello slider.
    Took 2 minutes

--- lib/interface/widget/article_slider_element.dart
+++ lib/interface/widget/article_slider_element.dart
@@ -1,3 +1,4 @@
+import 'package:app/interface/screen/article_detail_screen.dart';
 import 'package:app/models/article_model.dart';
 import 'package:app/models/preferences_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -12,31 +13,35 @@ class ArticleSliderElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 7 / 5,
-      child: Container(
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
-              child: CachedNetworkImage(
-                imageUrl: article.featuredMediaUrl,
-                fit: BoxFit.cover,
-                placeholder: (BuildContext context, String imageUrl) => Center(child: CircularProgressIndicator()),
-              ),
-            ),
-            Align(
-              alignment: AlignmentDirectional.bottomStart,
-              child: Padding(
-                padding: EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
-                child: Text(
-                  article.title,
-                  style: Theme.of(context).textTheme.headline4.copyWith(color: Colors.white),
+    return InkWell(
+      onTap: () =>
+          Navigator.of(context).pushNamed(ArticleDetailScreen.route, arguments: ArticleDetailScreenRequest(article: article, preferences: preferences)),
+      child: AspectRatio(
+        aspectRatio: 7 / 5,
+        child: Container(
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              ColorFiltered(
+                colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+                child: CachedNetworkImage(
+                  imageUrl: article.featuredMediaUrl,
+                  fit: BoxFit.cover,
+                  placeholder: (BuildContext context, String imageUrl) => Center(child: CircularProgressIndicator()),
                 ),
               ),
-            ),
-          ],
+              Align(
+                alignment: AlignmentDirectional.bottomStart,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8.0, bottom: 8.0, right: 8.0),
+                  child: Text(
+                    article.title,
+                    style: Theme.of(context).textTheme.headline4.copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
